0b4a454474673bdcb813a26166402cfdbd499983
Aymen <76121581+aymks@users.noreply.github.com>
2022-06-19 06:01:13 +0100

    1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.scss";
+import { Gradient } from "whatamesh";
+
+const gradient = new Gradient();
+gradient.initGradient("#gradient-canvas");
 
 function App() {
-  return <div className="App">saslo</div>;
+  return (
+    <div className="App">
+      <canvas id="gradient-canvas"></canvas>
+      <div className="txt">Hesssllo</div>
+    </div>
+  );
 }
 
 export default App;
