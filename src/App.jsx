b1054d9a050520f18d8f2cb81ebe73b9a1a30f8d
Aymen <76121581+aymks@users.noreply.github.com>
2022-06-20 03:53:19 +0100

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import PinterestLayout from "./components/PinterestLayout";
+
+
 function App() {
   
   return (
       <div className="App">
-         
+          
       <div className="txt">My Gallery</div>
       <div className="container" >
       <PinterestLayout/>
